469d850b4ae7537aa0e808fbc76cce84d0af43f9
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-08-21 15:22:49 +0530

    update code

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Policy from "./components/pages/Policy";
+import Pagenotfound from "./components/pages/Pagenotfound";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
